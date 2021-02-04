a8f6785095018c18972090453f92ec595e731291
eldaroid <eldario10@mail.ru>
2021-02-05 01:38:57 +0300

    done

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -11,15 +11,32 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+    
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default: break
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        var count = 0
-//        for _ in 1...36 {
-//            count += 1
-//             if let card = deck.draw() {
-//                 print("\(count) = \(card)")
-//             }
-        // Do any additional setup after loading the view.
-//        }
+        
     }
 }
